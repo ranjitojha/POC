35a65e91dc82816b06c0d2ada88427fa1193097d
ranjitojha <ranjitojha@gmail.com>
2023-04-12 01:53:14 +0530

    Update Steganography.java

--- demo/src/main/java/com/example/demo/controller/Steganography.java
+++ demo/src/main/java/com/example/demo/controller/Steganography.java
@@ -5,7 +5,15 @@ import java.awt.*;
 import java.awt.image.*;
 import javax.imageio.*;
 import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 /*import java.awt.image.BufferedImage;
@@ -17,13 +25,14 @@ public class Steganography {
 
   // embed secret information/TEXT into a "cover image"\
 	public static File encode(BufferedImage image, String text) {
+		System.out.println("Generated Token : "+text);
 		File file = null;
 		int bitMask = 0x00000001;	// define the mask bit used to get the digit\
 		int bit;				// define a integer number to represent the ASCII number of a character\
 		int x = 0;				// define the starting pixel x\
 		int y = 0;				// define the starting pixel y\
 		for(int i = 0; i < text.length(); i++) {
-			System.out.println("charAt  "+i +" : "+text.charAt(i));
+			//System.out.println("charAt  "+i +" : "+text.charAt(i));
 			bit = (int) text.charAt(i);		// get the ASCII number of a character\
 			for(int j = 0; j < 8; j++) {
 				int flag = bit & bitMask;	// get 1 digit from the character\
@@ -55,12 +64,46 @@ public class Steganography {
 		
 		// save the image which contains the secret information to another image file\
 		try {
-			 file = new File("textEmbedded.JPG");	
-			ImageIO.write(image, "png", file);	
-		} catch (IOException e) {
 			
+			file = File.createTempFile("textEmbedded-", ".jpg");
+			ImageIO.write(image, "jpg", file);
+			String absolutePath = file.getAbsolutePath();
+            String tempFilePath = absolutePath
+                  .substring(0, absolutePath.lastIndexOf(File.separator));
+            
+            System.out.println("Temp absolutePath path : " + absolutePath);
+            System.out.println("Temp file path : " + tempFilePath);
+            System.out.println("Temp file size : " + file.length());
+            System.out.println("Temp file Nme : " + file.getName());
+            System.out.println("Temp file path : " + file.getPath());
+            
+			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}		
 		return file;
+	}
+	
+public static void decode() {
+		
+		try {
+			Path path = Paths.get(System.getProperty("java.io.tmpdir")+"/tempTest.txt");
+			Stream<String> lines = null;
+			try {
+				lines = Files.lines(path);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    String data = lines.collect(Collectors.joining("\n"));
+		    System.out.println("Data "+data);
+		    lines.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		         
+		    
 	}
 	
 	// extract secret information/Text from a "cover image"\
@@ -134,4 +177,6 @@ public class Steganography {
 			
 		}		
 	}
+	
+	
 }
